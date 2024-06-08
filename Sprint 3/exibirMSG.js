8224b7615e1bd568a40e3b74b1829f9c246db1bd
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-06-08 20:56:09 -0300

    l

--- Sprint 3/exibirMSG.js
+++ Sprint 3/exibirMSG.js
@@ -1,6 +1,6 @@
 const divMensagens = document.querySelector("#msgs");
-const receitaSelecionada = 1; 
-
+const receitaSelecionada = 1; //Caso queira testar a exibição, altere esse valor para outro número - Essa parte depende da sprint 3 de outro integrante
+//Não se esqueça de alterar o id da receita no db.json
 fetch("db.json").then((response) => {
     response.json().then((dados) => {
         const mensagensFiltradas = dados.msgs.filter((mensagem) => {
