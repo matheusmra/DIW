d4b013ad4c5c6aaeb937a03765cf78c418786a35
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-06-25 14:32:55 -0300

    Update github.js

--- Trabalho Prático 01/github.js
+++ Trabalho Prático 01/github.js
@@ -1,29 +1,27 @@
-// Script para carregar os dados do perfil do usuário a partir do GitHub
+
 $.getJSON('https://api.github.com/users/matheusmra', function(data) {
-    // Atualiza o nome do usuário com o nome obtido da API do GitHub
+
     $('#nomeUsuario').text(data.name);
     $('#descricaoUsuario').text(data.bio);
     $('#imagemPerfil').attr('src', data.avatar_url);
 
-    // ... (o restante do seu código para atualizar outros elementos do perfil)
+
 });
 
 $.getJSON('https://api.github.com/users/alicesalim', function(data) {
-    // Atualiza o nome do usuário com o nome obtido da API do GitHub
+
     $('#alice').text(data.name);
     $('#descalice').text(data.bio);
     $('#imagemAlice').attr('src', data.avatar_url);
 
-    // ... (o restante do seu código para atualizar outros elementos do perfil)
 });
 
 $.getJSON('https://api.github.com/users/iasminfeo', function(data) {
-    // Atualiza o nome do usuário com o nome obtido da API do GitHub
+
     $('#iasmin').text(data.name);
     $('#desciasmin').text(data.bio);
     $('#imagemIasmin').attr('src', data.avatar_url);
 
-    // ... (o restante do seu código para atualizar outros elementos do perfil)
 });
 
 $.getJSON('https://api.github.com/users/ga-amaral2', function(data) {
@@ -31,13 +29,12 @@ $.getJSON('https://api.github.com/users/ga-amaral2', function(data) {
     $('#descgabriel').text(data.bio);
     $('#imagemGabriel').attr('src', data.avatar_url);
 
-    // ... (o restante do seu código para atualizar outros elementos do perfil)
+
 });
 
-// Substitua 'USERNAME' pelo seu nome de usuário do GitHub
 const username = 'matheusmra';
 
-// Função para criar um card de repositório
+
 function createRepoCard(repo) {
     return `
         <div class="col">
@@ -54,14 +51,12 @@ function createRepoCard(repo) {
     `;
 }
 
-// Função para carregar os repositórios do GitHub e adicionar ao HTML
+
 function loadGitHubRepos() {
     $.getJSON(`https://api.github.com/users/matheusmra/repos`, function(repos) {
         const reposHTML = repos.map(createRepoCard).join('');
-        // Substitua 'repo-container' pelo ID do elemento onde você quer inserir os cards
+
         $('#repositorios').html(reposHTML);
     });
 }
-
-// Chama a função quando o documento estiver pronto
 $(document).ready(loadGitHubRepos);
